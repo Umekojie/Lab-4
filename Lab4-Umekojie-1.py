9307cc54d44212a5c5f2937d11e6e5ad53c9f0b2
Josephine Cattell <josieohio7967@gmail.com>
2026-02-08 23:35:42 -0500

    add deck function

--- Lab4-Umekojie-1.py
+++ Lab4-Umekojie-1.py
@@ -8,3 +8,7 @@ print("Enter number of cards per hand")
 hand_size = int(input())
 values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 suits = ["c", "d", "h", "s"]
+deck = []
+for value in values:
+    for suit in suits:
+        deck.append(value + suit)
