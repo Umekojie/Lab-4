d6bc8e4f5cb0fe181529cd2d5af24e9d298d4c92
Josephine Cattell <josieohio7967@gmail.com>
2026-02-08 23:21:02 -0500

    add values and suits

--- Lab4-Umekojie-1.py
+++ Lab4-Umekojie-1.py
@@ -5,3 +5,6 @@ the purppose for this program is to simulate dealing a hand of cards
 
 import random 
 print("Enter number of cards per hand")
+hand_size = int(input())
+values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+suits = ["c", "d", "h", "s"]
